7bec9882b90f701661d7e5f3cde6c14d67bcbf9b
anupk <anupk@NIBC2087>
2017-09-04 12:08:27 +0530

    Add project files.

---
+++ SampApp/SampApp/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using Xamarin.Forms;
+
+namespace SampApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SampApp.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            //MobileCenter.Start("android=5750b40d-91dc-455e-8c0d-47ff4778ffe9;"
+            //    //+
+            //    //    "uwp={Your UWP App secret here};" +
+            //    //    "ios={Your iOS App secret here}",
+            //        ,typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ SampApp/SampApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void timelineListView_ItemTapped(object sender,EventArgs e)
+        {
+
+        }
+    }
+}
